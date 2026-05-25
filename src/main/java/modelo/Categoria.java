eb3e2ad4d6b1277629344d0a3f684bc9e0b46624
yTwooh <juantube.gamer@gmail.com>
2026-05-25 12:37:58 -0300

    feat: Implementação CRUD Categoria

--- src/main/java/modelo/Categoria.java
+++ src/main/java/modelo/Categoria.java
@@ -1,7 +1,62 @@
 
 package modelo;
 
-
 public class Categoria {
-    
+
+    private int idCategoria;
+    private String nome;
+    private String tamanho;
+    private String embalagem;
+
+    public Categoria() {
+        this.idCategoria = 0;
+        this.nome = "";
+        this.tamanho = "";
+        this.embalagem = "";
+    }
+
+    public Categoria(int idCategoria, String nome, String tamanho, String embalagem) {
+        this.idCategoria = idCategoria;
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.embalagem = embalagem;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
+
+    public void setEmbalagem(String embalagem) {
+        this.embalagem = embalagem;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
 }
