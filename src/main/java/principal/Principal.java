5575471c7571bd102b4e505c07e08dc4dd54336b
Deise Cristine Souza Soares <dcristine.estudos@gmail.com>
2026-05-28 11:08:46 -0300

    feat: configura classe principal para iniciar o sistema

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,35 +1,13 @@
 package principal;
 
-public class Principal {
-
-    public static void main(String[] args) {
-        System.out.println("Alo Mundo!");
-    }
-}
-
-/*package principal;
-
-import dao.Conexao;
-
-public class Principal {
-
-    public static void main(String[] args) {
-
-        Conexao.conectar();
-
-    }
-
-}*/
- /*
-package principal;
-
-
 import visao.FrmMenuPrincipal;
 
 public class Principal {
 
     public static void main(String[] args) {
-        FrmMenuPrincipal tela = new FrmMenuPrincipal();
-        tela.setVisible(true);
+
+        FrmMenuPrincipal menu = new FrmMenuPrincipal();
+        menu.setVisible(true);
+
     }
-}*/
+}
