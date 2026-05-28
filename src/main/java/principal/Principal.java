47f9edec0c7811e6ca57cc1bbd6edf9879eb631e
Deise Cristine Souza Soares <dcristine.estudos@gmail.com>
2026-05-28 19:48:15 -0300

    docs: adiciona Javadoc da classe Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -2,8 +2,22 @@ package principal;
 
 import visao.FrmMenuPrincipal;
 
+/**
+ * Classe principal do sistema de controle de estoque. Responsável por iniciar a
+ * aplicação e exibir a tela de menu principal.
+ *
+ * @author Deise Cristine Souza Soares
+ * @version 1.0
+ * @see visao.FrmMenuPrincipal
+ */
 public class Principal {
 
+    /**
+     * Método principal da aplicação. Inicializa o sistema e exibe o menu
+     * principal.
+     *
+     * @param args argumentos da linha de comando
+     */
     public static void main(String[] args) {
 
         FrmMenuPrincipal menu = new FrmMenuPrincipal();
