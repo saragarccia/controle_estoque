b830746f09585acc850e288e4acf77a8cf9f3f77
Victoria Pires <viic.pirexx18@gmail.com>
2026-05-27 22:31:04 -0300

    feat: fazendo conexão da movimentação com banco

--- src/main/java/dao/MovimentacaoDAO.java
+++ src/main/java/dao/MovimentacaoDAO.java
@@ -1,13 +1,43 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import modelo.Movimentacao;
-import javax.swing.JOptionPane;
 
 public class MovimentacaoDAO {
 
     public void salvar(Movimentacao mov) {
 
-        JOptionPane.showMessageDialog(null,
-                "Salvo no banco!\nProduto: " + mov.getProduto());
+        String sql = "INSERT INTO movimentacao "
+                + "(id_produto, data_movimentacao, quantidade_movimentada, tipo_movimentacao) "
+                + "VALUES (?, ?, ?, ?)";
+
+        try {
+
+            Connection conexao = Conexao.conectar();
+
+            PreparedStatement comando = conexao.prepareStatement(sql);
+
+            comando.setInt(1, Integer.parseInt(mov.getProduto()));
+
+            comando.setString(2, mov.getData());
+
+            comando.setInt(3, mov.getQuantidade());
+
+            comando.setString(4, mov.getTipo());
+
+            comando.executeUpdate();
+
+            comando.close();
+            conexao.close();
+
+            javax.swing.JOptionPane.showMessageDialog(null,
+                    "Movimentação salva no banco!");
+
+        } catch (Exception e) {
+
+            javax.swing.JOptionPane.showMessageDialog(null,
+                    "Erro ao salvar: " + e.getMessage());
+        }
     }
 }
