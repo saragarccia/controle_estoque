51b20f3db2d20d2c36db69a16159b0564d27133f
Deise Cristine Souza Soares <dcristine.estudos@gmail.com>
2026-05-29 10:30:29 -0300

    docs: adiciona JavaDoc da classe Conexao

--- src/main/java/dao/Conexao.java
+++ src/main/java/dao/Conexao.java
@@ -3,16 +3,27 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+ * Classe responsável por estabelecer a conexão com o banco de dados MySQL do
+ * sistema de controle de estoque.
+ *
+ * @author Deise Cristine Souza Soares
+ * @version 1.0
+ */
 public class Conexao {
 
+    /**
+     * Realiza a conexão com o banco de dados do sistema.
+     *
+     *@return conexão ativa com o banco de dados ou {@code null} em caso de erro
+     */
     public static Connection conectar() {
 
         try {
 
             String url = "jdbc:mysql://localhost:3306/sistema_estoque";
             String usuario = "root";
-            String senha = "Stu.pp2121@";
-            
+            String senha = "Mysql@1312";
 
             Connection conexao = DriverManager.getConnection(url, usuario, senha);
 
